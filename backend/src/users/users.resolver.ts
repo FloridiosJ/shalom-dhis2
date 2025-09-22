fcdc26f58de7a8c3ad1dcb8665a1f9b16170ab70
FloridiosJacob <jacob@hairun-technology.com>
2025-09-22 12:04:04 +0300

    crud user

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -1,4 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { CreateUserInput } from './dto/create-user.input';
 
-@Resolver()
-export class UsersResolver {}
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private usersService: UsersService) {}
+
+  @Query(() => [User])
+  users(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Query(() => User)
+  user(@Args('id', { type: () => Int }) id: number): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('input') input: CreateUserInput): Promise<User> {
+    const { email, password, role } = input;
+    return this.usersService.create(email, password, role);
+  }
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) throw new NotFoundException(`User #${id} not found`);
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async create(email: string, password: string, role: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.usersRepository.create({ email, password: hashedPassword, role });
+    return this.usersRepository.save(user);
+  }
+
+  async update(id: number, attrs: Partial<User>): Promise<User> {
+    const user = await this.findOne(id);
+    if (attrs.password) {
+      attrs.password = await bcrypt.hash(attrs.password, 10);
+    }
+    Object.assign(user, attrs);
+    return this.usersRepository.save(user);
+  }
+
+  async delete(id: number): Promise<void> {
+    const result = await this.usersRepository.delete(id);
+    if (result.affected === 0) throw new NotFoundException(`User #${id} not found`);
+  }
+}
