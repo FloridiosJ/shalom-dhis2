7b607d6230a9248f4d96785d0ddd442b1f83e146
FloridiosJacob <jacob@hairun-technology.com>
2025-09-27 15:05:53 +0300

    login from front-end

--- frontend-web/src/main.tsx
+++ frontend-web/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom'
+import { client } from './lib/apolloClient'
+import App from './App'
+import './index.css'  // Cette ligne est importante !
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>,
 )
---
+++ frontend-web/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+export function Dashboard() {
+  return (
+    <div className="p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Bienvenue sur le dashboard
+      </h1>
+    </div>
+  );
+}
