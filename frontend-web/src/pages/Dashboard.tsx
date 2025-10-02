77428ac4509e0923788ce7932521a9a38af3ee4f
FloridiosJacob <jacob@hairun-technology.com>
2025-10-02 10:36:50 +0300

    reset project

--- frontend-web/src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-export function Dashboard() {
-  return (
-    <div className="p-8 bg-gray-100 min-h-screen">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Bienvenue sur le dashboard
-      </h1>
-    </div>
-  );
-}
